6dcc3ebc88408813039ef16555204487e8ee49d5
Sujit <sujit.mollet@rutgers.edu>
2021-10-01 01:49:16 -0400

    Done with Week 2

---
+++ Week 2/TipCalculatorf2021/TipCalculatorf2021/TipCalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TipCalculatorViewController.swift
+//  TipCalculatorf2021
+//
+//  Created by Sujit Molleti on 9/29/21.
+//
+
+import UIKit
+
+class TipCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
